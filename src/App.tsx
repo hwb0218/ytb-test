9e024d863f30914cd6c512158018ca130c5855ac
FEWoobIn <hwb0218@gmail.com>
2023-11-25 04:59:46 +0900

    라우터 프로바이더

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-const App = () => {
-  return <div>hello!!</div>;
-};
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import routes from './routes';
+
+const router = createBrowserRouter(routes);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
 
 export default App;
