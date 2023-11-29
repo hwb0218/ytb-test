043e3fc70dadb1689fe51cf8eec9c2081f338e04
FEWoobIn <hwb0218@gmail.com>
2023-11-29 17:17:13 +0900

    헤더 fixed 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import SearchBar from './searchbar';
 
 export default function Header() {
   return (
-    <header className="p-2 flex justify-between items-center bg-menuDark">
+    <header className="fixed h-[60px] w-full p-2 flex justify-between items-center bg-menuDark">
       <Link to={{ pathname: '/' }} className="flex items-center cursor-pointer">
         <FaYoutube className="text-4xl text-brandRed" />
         <h1 className="text-2xl">Youtube</h1>
