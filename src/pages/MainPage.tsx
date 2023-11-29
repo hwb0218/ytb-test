7fd5add49707f19477c556ea4987a2349d9d451f
FEWoobIn <hwb0218@gmail.com>
2023-11-29 17:19:21 +0900

    suspense 제거

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,10 @@
-import { Suspense } from 'react';
 import Wrapper from '../components/common/wrapper';
 import VideoList from '../components/videos/VideoList';
 
 export default function MainPage() {
   return (
-    <Wrapper className="p-2">
-      <Suspense fallback={<p>로딩 중...</p>}>
-        <VideoList />
-      </Suspense>
+    <Wrapper className="p-2 max-h-[100vh] pt-[68px]">
+      <VideoList />
     </Wrapper>
   );
 }
